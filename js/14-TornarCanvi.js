5f821dd426107658c229a9f1f62e857f8f48d54f
Jordi Manyà <2023_jordi.manya@iticbcn.cat>
2023-10-06 17:11:42 +0200

    exercici 14

--- js/14-TornarCanvi.js
+++ js/14-TornarCanvi.js
@@ -4,15 +4,29 @@
  */
 
 function canvi() {
-    let preu = parseInt(prompt("Digues un preu: ")),
-        diners = parseInt(prompt("Ara digues quan pagaràs (ha de ser més que el preu): "));
+    let preu = parseFloat(prompt("Digues un preu: ")),
+        diners = parseFloat(prompt("Ara digues quan pagaràs (ha de ser més que el preu): "));
 
     while (isNaN(preu) || isNaN(diners)) {
         alert("número no vàlid...");
-        preu = parseInt(prompt("Digues un preu: ")),
-        diners = parseInt(prompt("Ara digues quan pagaràs (ha de ser més que el preu): "));
+        preu = parseFloat(prompt("Digues un preu: ")),
+        diners = parseFloat(prompt("Ara digues quan pagaràs (ha de ser més que el preu): "));
     }
 
+    console.log("Preu a pagar: "+preu+"\nQuantitat pagada: "+diners);
+
+    let euros = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
+
+    let canvi = diners - preu;
+    canvi.toFixed(2);
+    let residu = canvi;
     
+    for (let i=0; i < euros.length; i++) {
+        if(parseInt(canvi/euros[i]) > 0) {
+            console.log(parseInt(canvi / euros[i])+" de "+euros[i]);
+            canvi = canvi%euros[i];
+        }
+    }
+    console.log("Total canvi: "+residu);
 
 }
